4ae5b4a834afabe160bb954377a02acc7d87086f
Yadwinder Kaur <31966849+yadwink@users.noreply.github.com>
2022-11-10 20:37:03 +0100

    Update predict-test.py

--- Mid-Term Project/predict-test.py
+++ Mid-Term Project/predict-test.py
@@ -1,7 +1,6 @@
-host = 'Livercirrhosis-env.eba-rii2zsem.us-east-1.elasticbeanstalk.com'
 import requests
 
-url = f'http://{host}/predict/'
+url = 'http://127.0.0.1:5000/predict/'
 
 patient_id = 'patient-abc123'
 
